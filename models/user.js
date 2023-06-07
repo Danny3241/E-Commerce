2d6a2d8b8a9affc581045c6ae44fa42de1da837e
Danny3241 <sakhiyadenish33@gmailcom>
2023-06-07 08:20:11 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs")
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    street: {
+        type: String,
+        default: ''
+    },
+    apartment: {
+        type: String,
+        default: ''
+    },
+    zip: {
+        type: String,
+        default: ''
+    },
+    city: {
+        type: String,
+        default: ''
+    },
+    country: {
+        type: String,
+        default: ''
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now 
+    },
+})
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+//~Compare Password
+userSchema.methods.comparePassword = async function (dbPassword, userPassword) {
+    return await bcrypt.compare(dbPassword, userPassword);
+};
+
+
+
+
+module.exports = mongoose.model("User", userSchema);
+
---
+++ service/userService.js
@@ -0,0 +1,35 @@
+const User = require('../models/user')
+
+const createUser = async (user) => {
+    console.log(property)
+    return await Property.create(property)
+}
+
+const getUser = async(user)=>{
+    return await Property.findOne(user)
+}
+
+const getAllProperty = async () => {
+    return await Property.find()
+}
+
+const getPropertyById = async (id) => {
+    return await Property.findById(id)
+}
+
+const updateProperty = async (id, property) => {
+    return await Property.findByIdAndUpdate(id, property)
+}
+
+const deleteProperty = async (id) => {
+    return await Property.findByIdAndDelete(id)
+}
+
+module.exports = {
+    createProperty,
+    getUser,
+    getAllProperty,
+    getPropertyById,
+    updateProperty,
+    deleteProperty
+}
