326e7ec06e870967f81316a0edbaa5cb8e8683f3
Danny3241 <sakhiyadenish33@gmail.com>
2023-06-23 14:44:36 +0530

    aww

--- app.js
+++ app.js
@@ -7,7 +7,48 @@ const orderRouter = require("./routes/orderRoutes");
 const orderItemRouter = require("./routes/orderItemRoutes");
 const userLoginRouter = require("./routes/userLoginRoutes");
 const discountRouter = require("./routes/discountRoutes");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const app = express();
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Ecommerce Product Information",
+    version: "1.0.0",
+    description: " Information about the product",
+  },
+  servers: [
+    {
+      url: "http://localhost:5656/",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      jwt: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+      },
+    },
+  },
+  security: [
+    {
+      jwt: [],
+    },
+  ],
+};
+
+// Options for the Swagger definition
+const option = {
+  swaggerDefinition: swaggerDefinition,
+  // Path to the API docs
+  //apis: ["./routes/propertyRoute.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(option);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // require('dotenv/config');
 // const authJwt = require('./helpers/jwt');
@@ -15,7 +56,7 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // app.use(express.static(__dirname + '/public'));
 // app.use('/api/uploads', express.static('./uploads'));
@@ -26,26 +67,27 @@ app.use("/api/category", categoryRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/orderItem", orderItemRouter);
 app.use("/api/userLogin", userLoginRouter);
-app.use("/api/discount", discountRouter)
+app.use("/api/discount", discountRouter);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("hello, this is homepage.");
-})
+});
 
 app.listen(5656, () => {
   console.log("Server is running on port 5656");
 });
 
-const url = 'mongodb://0.0.0.0:27017/E-Commerce';
+const url = "mongodb://0.0.0.0:27017/E-Commerce";
 const options = {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}
+  useUnifiedTopology: true,
+};
 mongoose.Promise = global.Promise;
-mongoose.connect(url, options)
-  .then(() => { console.log("Connected to the MongoDB successfully done") })
+mongoose
+  .connect(url, options)
+  .then(() => {
+    console.log("Connected to the MongoDB successfully done");
+  })
   .catch((err) => console.log(err));
 
-
 module.exports = app;
-
